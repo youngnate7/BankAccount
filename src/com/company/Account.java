5544f209ecf5ea0ab97f3c4c251443af2a047f8b
Nathan <nathanyoung@billericak12.com>
2020-02-26 11:06:53 -0500

    bank

---
+++ src/com/company/Account.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public abstract class Account {
+    private int idNum;
+    private double balance;
+
+
+    public Account(int id, double startBal)
+    {
+        idNum = id;
+        balance = startBal;
+    }
+
+    public int idNumber()a
+    {
+        return idNum;
+    }
+
+    public double currentBalance()
+    {
+        return balance;
+    }
+
+    public void deposit(double amount)
+    {
+        balance += amount;
+    }
+    public void decreaseBalance(double amount)
+    {
+        balance -=amount;
+    }
+
+    public abstract double monthlyInterest();
+
+
+}
