ea11e362024aba4ca182fa1636e7c2c491d13930
Nathan <nathanyoung@billericak12.com>
2020-02-26 12:28:21 -0500

    bank

--- src/com/company/SavingsAccount.java
+++ src/com/company/SavingsAccount.java
@@ -1,7 +1,17 @@
 package com.company;
 
 public class SavingsAccount extends Account {
-    public SavingsAccount(int idNumber, double startBal){
+    double annualInterest;
+    public SavingsAccount(int idNumber, double startBal, double ai){
         super(idNumber, startBal);
+        annualInterest = ai;
+    }
+    public void withdraw(double w){
+        if(w <= currentBalance()){
+            super.decreaseBalance(w);
+        }
+    }
+    public double monthlyInterest(){
+        
     }
 }
